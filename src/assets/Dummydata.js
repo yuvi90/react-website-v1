a8f128c1981f665a3fcee2eceeb062787472817c
Yuvraj <ygill63@gmail.com>
2022-10-14 17:08:36 +0530

    data add

---
+++ src/assets/Dummydata.js
@@ -0,0 +1,42 @@
+const dummyData = [
+    {
+        id: 1,
+        title: "Web Development",
+        image: "https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi at quae et aperiam ipsa culpa a sit dolorum beatae",
+    },
+
+    {
+        id: 2,
+        title: "App Development",
+        image: "https://images.pexels.com/photos/1181244/pexels-photo-1181244.jpeg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi at quae et aperiam ipsa culpa a sit dolorum beatae",
+    },
+    {
+        id: 3,
+        title: "Software Development",
+        image: "https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipislting elit. Modi at quae et aperiam ipsa culpa a sit dolorum beatae",
+    },
+
+    {
+        id: 4,
+        title: "Digital Marketing",
+        image: "https://images.pexels.com/photos/905163/pexels-photo-905163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi at quae et aperiam ipsa culpa a sit dolorum beatae"
+    },
+
+    {
+        id: 5,
+        title: "Graphic Designer",
+        image: "https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi at quae et aperiam ipsa culpa a sit dolorum beatae"
+    },
+
+    {
+        id: 6,
+        title: "Thapa Codefest",
+        image: "https://thapaonlineclass.com/images/thapa.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi at quae et aperiam ipsa culpa a sit dolorum beatae"
+    }
+];
